f81b37a08dc819c5a36f2e7f96896d43cded27e1
Jatin Chavda <jatinchavda@email.com>
2025-07-04 15:20:46 +0530

    Initial commit with Frontend and Backend

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.get("/", (req, res) => res.send("Hello from backend!"));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
